2dd7f1474c5ae089ab32ef57b03471149eeebe58
cosrnic <75631523+cosrnic@users.noreply.github.com>
2023-01-08 18:48:00 +0000

    Fix /aes

--- src/commands/tools/aes.js
+++ src/commands/tools/aes.js
@@ -10,15 +10,36 @@ module.exports = {
             fetchReply: true,
         });
 
-        const aes = await axios.get(
-            "https://fortnitecentral.gmatrixgames.ga/api/v1/aes"
-        );
-        const key = aes.data.mainKey;
-        const ver = aes.data.version;
+        const aes = await axios.get("https://fortnite-api.com/v2/aes");
+        const key = aes.data.data.mainKey;
+        const ver = aes.data.data.build;
+
         const embed = new EmbedBuilder()
             .setColor("#7F98E6")
             .setTitle("Aes Key for " + ver)
-            .setDescription("`" + key + "`");
+            .addFields({
+                name: "Key",
+                value: "`" + key + "`",
+            });
+
+        for (let i = 0; i < aes.data.data.dynamicKeys.length; i++) {
+            if (i >= 24) break;
+
+            if (aes.data.data.dynamicKeys[i].pakFilename.includes("optional"))
+                break;
+
+            if (
+                i > 0 &&
+                aes.data.data.dynamicKeys[i].key ==
+                    aes.data.data.dynamicKeys[i - 1].key
+            ) {
+                embed.addFields({
+                    name: aes.data.data.dynamicKeys[i].pakFilename,
+                    value: "`" + aes.data.data.dynamicKeys[i].key + "`",
+                });
+            }
+        }
+
         interaction.editReply({
             embeds: [embed],
         });
